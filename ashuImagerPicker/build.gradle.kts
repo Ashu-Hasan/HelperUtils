9dc3a6a5937d38e545b9ff05e76751426704480f
Mr. Ash <ashu.hasan.155221@gmail.com>
2026-05-03 16:00:54 +0530

    1.0.2

--- ashuImagerPicker/build.gradle.kts
+++ ashuImagerPicker/build.gradle.kts
@@ -43,7 +43,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.github.Ashu-Hasan"   // GitHub username
                 artifactId = "HelperUtils"             // Library name
-                version = "1.0.1"                   // Match your Git tag
+                version = "1.0.2"                   // Match your Git tag
                 from(components["release"])
 
                 pom {
